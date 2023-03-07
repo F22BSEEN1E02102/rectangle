dec01cac431b00353043129a08d96bfb79cd63e9
Muhammad Arif <123452078+F22BSEEN1E02102@users.noreply.github.com>
2023-03-07 11:58:01 +0500

    Add files via upload

---
+++ rectangulare.py
@@ -0,0 +1,12 @@
+class Rectangle:
+    def __init__(self,height,width):
+        self.height=height
+        self.width=width
+
+rect1=Rectangle(20,30)    
+rect2=Rectangle(40,43)
+
+print(rect1.height *rect1.width)
+print(rect2.height *rect2.width)
+
+
